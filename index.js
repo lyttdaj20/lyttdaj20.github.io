8c974e603d53fd15645881de11b7b83ab583fda5
Dajan L <62118597+lyttdaj20@users.noreply.github.com>
2023-12-24 23:13:04 -0800

    Header/Footer index.js

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function loadHTML(id, filename) {
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4) {
                 if (this.status == 200) {element.innerHTML = this.responseText;}
-                if (this.status == 404) {element.innerHTML = "<h1>Page not found! y</h1>";}
+                if (this.status == 404) {element.innerHTML = "<h1>Page not found!</h1>";}
             }
         }
-        xhttp.open("GET", `template/${file}`, true);
+        xhttp.open("GET", file, true);
         xhttp.send();
         return;
     }
